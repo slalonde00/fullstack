df22f87c92b8231f2baee33041ba6ae4f2740866
slalonde00 <slalonde00@gmail.com>
2020-05-29 20:25:26 -0400

    fixing route try #3

--- express/routes/count.js
+++ express/routes/count.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 
+var fs = require('fs');
+
+let file = fs.readFileSync('./quotes.json').toString();
+let lines = file.split('}');
+
+function countQuotes(filename = '') {
+    fs.existsSync(filename)
+    let seperateur = ','
+    calculSeparateurNumber = (file.lastIndexOf('}'));
+    return (calculSeparateurNumber);
+}
+
+
 router.get('/count', function(req, res, next) {
-    res.render('count',{ title: 'Express' });
+    res.render('count', {title= title, fs: {echo: countQuotes('./quotes.json')}});
   });
   module.exports = router;
